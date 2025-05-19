6198bac318344c396abde0894b1cc65447af5ed3
Mehak <122101735+MehakVarshney@users.noreply.github.com>
2025-05-19 06:33:06 +0000

    Discusseion in datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+//alert(3+3) // we are using node JS not browser
+
+console.log(3
+    
+    +
+    3); //code readability should be high
+
+let name = "mehak"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string "",''
+// boolean => true/false
+// null => Stand alone value - representation of empty value
+// undefined =>when you have only declared the value not assigned
+// symbol => to fing uniqueness
+
+// object
+
+console.log(typeof(age));// number
+console.log(typeof "mehak"); //string
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
