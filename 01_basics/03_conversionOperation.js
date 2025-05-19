4428e41512a5f02a61af38fc7da5b5afde81f54e
Mehak <122101735+MehakVarshney@users.noreply.github.com>
2025-05-19 06:33:25 +0000

    conversion and operation on datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,74 @@
+// let score = "33abc"
+/*if score is null then valueInNumber is 0, 
+ if score is undefined the valueInNumber is NaN
+ if true then 1
+ 33abc = NaN
+ "33" = 33
+*/ 
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score) //string change to number
+
+// console.log(typeof valueInNumber);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber); //NaN - Not a number
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); //true
+// 1 = true 
+// 0 = false
+// "" = false
+// "mehak" = true
+
+
+// let someNumber = 35 // number
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber); // string
+
+// *********************** Operations *****************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); //pwer 2^2
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " mehak"
+let str3 = str1+str2;
+// console.log(str3);
+
+// console.log("1"+2);// 12
+// console.log(1+"2");// 12
+// console.log("1"+2+2);// 122
+// console.log(1+2+"2"); // 32
+
+
+// console.log(+true); //1
+// console.log(true+); // not possible
+
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
