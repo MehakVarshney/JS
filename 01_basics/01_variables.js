39c49a7299fa922fa6f62ca5fa15fe00dca70870
Mehak <122101735+MehakVarshney@users.noreply.github.com>
2025-05-19 05:28:57 +0000

    discussion on variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accountEmail = "mehak00@gmail.com"
+var accountPass = "12345"
+accountCity = "jaipur"
+
+let accountState;
+
+// accountId = 2 not allowed
+accountEmail = "kmnhyufrtf@gmail.com"
+accountPass = "98765"
+accountCity = "bilsi"
+console.log(accountId);
+
+/*
+Prefer not to use var 
+Because of issue of block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPass,accountCity, accountState])
